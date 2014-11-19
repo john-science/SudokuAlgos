580f67de03ea1fcfe793baeedb66d01c19761e6c
theJollySin <thejollysin@gmail.com>
2014-11-19 12:05:09 -0800

    placeholder for terminal pattern creator

---
+++ src/main/java/net/antineutrino/SudokuAlgos/TerminalPattern.java
@@ -0,0 +1,14 @@
+package net.antineutrino.SudokuAlgos;
+
+public class TerminalPattern {
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static byte[][] create() {
+		byte[][] pattern = new byte[9][9];
+
+		return pattern;
+	}
+}
