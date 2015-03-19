71888ad42b7b0649226001672583e9c43755ce9c
John Stilley <thejollysin@gmail.com>
2015-03-19 12:48:51 -0700

    blueprint for necessary class

---
+++ src/main/java/net/antineutrino/SudokuAlgos/HasUniqueSolution.java
@@ -0,0 +1,14 @@
+package net.antineutrino.SudokuAlgos;
+
+
+public class HasUniqueSolution {
+
+	/**
+	* Determine if an incomplete Sudoku puzzle has a unique solution.
+	*/
+	public static boolean test(byte[][] current) {
+		// TODO: Everything
+		return false;
+	}
+
+}
