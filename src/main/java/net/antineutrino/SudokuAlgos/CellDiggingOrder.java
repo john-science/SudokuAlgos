e76f3cec6a06219da3f6345413a27cf9f34bdca9
John Stilley <thejollysin@gmail.com>
2015-03-19 13:03:10 -0700

    blueprint for necessary class

---
+++ src/main/java/net/antineutrino/SudokuAlgos/CellDiggingOrder.java
@@ -0,0 +1,22 @@
+package net.antineutrino.SudokuAlgos;
+
+public class CellDiggingOrder {
+
+	/**
+	* 
+	*/
+	public static int[][] random(byte[][] current, int difficulty) {
+		// TODO: Everything
+		return {};
+	}
+	
+	/**
+	* 
+	*/
+	public static int[][] serpentine(byte[][] current) {
+		// TODO: Everything
+		return {};
+	}
+	
+	// TODO: There should be ~4 orders in which to dig cells.
+}
