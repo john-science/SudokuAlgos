eadba57f165968e161528d5f911197348faaf573
John Stilley <thejollysin@gmail.com>
2015-03-19 13:08:34 -0700

    blueprint for necessary class

---
+++ src/main/java/net/antineutrino/SudokuAlgos/DifficultyLevelRestrictions.java
@@ -0,0 +1,12 @@
+package net.antineutrino.SudokuAlgos;
+
+public final class DifficultyLevelRestrictions {
+
+  /**
+  * 
+  */
+  public static int[] levelBased(int difficulty) {
+    // TODO: Everything
+    return {0, 0, 0};
+  }
+}
